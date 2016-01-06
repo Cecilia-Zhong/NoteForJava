5c11bfe1681fc9f1e747232379fb327702f0e5a2
l‘erable <erable@users.noreply.github.com>
2016-01-06 17:20:07 -0500

    Create lectureNote.java

---
+++ lectureNote.java
@@ -0,0 +1,37 @@
+/* Lecture 2 January 6th 
+* file I/O
+* Java tool: BlueJ;JDK plus text editor; Eclipse;
+* semi-colon  (;) is for separate but not the new line and the space;
+* i++ is means adding up 1 for i, int means integer --> for (int i = 1; i < n; i++ );
+* dynamic typing: must run program to determine the types of expressions 
+* java use static typing: declared with a type --> int x = 14;
+* a variable must be declared before you use !! (the variable would not compile until you declare)
+* 
+* 
+*/
+
+// create a new class, same as the name in file 
+public class helloWorld { 
+	// create the method named main 
+	public static void main(String[] args) {
+		// indentation problem 
+		/*
+		if ( n > 10){ 
+			a = 1;b = 2;
+		} // the whole brace { } is the group of statement 
+		// c = 3; 
+		 * 
+		 */
+
+	} // end main 
+
+} // end class helloWolrd
+
+/* question:
+ * 1. what is file I/O 
+ * 2. 
+ * 
+ */
+ 
+
+  
