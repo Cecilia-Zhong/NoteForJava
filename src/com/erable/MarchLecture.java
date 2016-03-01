6f0e29253c83bc4dae9def82bb87ea355211523e
moioutoi@163.com <moioutoi@163.com>
2016-03-01 16:46:06 -0500

    miaou

---
+++ src/com/erable/MarchLecture.java
@@ -0,0 +1,26 @@
+/* March 1st Tuesday 
+
+String name = (String) newStack;  // cast the name into string 
+int number = (Integer) number.pop(); 
+problem with cast: 2 --> 
+ArrayList: API --> growable array 
+can store any type of object 
+no error message for different type in array 
+ArrayList<Integer> = list of integrs --> 
+ArrayList<Employee> = list of employees 
+generic arraylist: connot contain other types 
+char firstChar() = strList.get(0).charAt(0); // first char, no cast 
+emPlist.get(0);  get 0 element 
+
+interface ???????
+implement ???????
+
+//e.g. 
+public interfaceComparable<T> {
+
+}
+
+import java.util.Array;
+
+
+
